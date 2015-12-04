82dadca1eccb1042adbb2acf1bf95e7a0ce5f602
David Caro <dcaroest@redhat.com>
2015-12-04 13:49:36 +0100

    Initial commit
    This includes a script to generate the repo:
    
      - bin/build.py that generates repo usable from current lago and
        virt-builder
    
    And the specs for a basic system of:
      - el6
      - el7
      - el7-nfs: same as el7 with nfs tools (layered)
      - el7-iscsi: same as el7 with iscsi tools (layered)
      - fc22
      - fc23
      - cirros
    
    Change-Id: I8b1cc067f335743f33d960ce4da9a64303d869a3
    Signed-off-by: David Caro <dcaroest@redhat.com>

---
+++ bin/depsolver.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""
+Based on the dependency resolution example by Mario Vilas (mvilas at gmail
+dot com)
+"""
+
+
+class Node(object):
+    def __init__(self, name, value, *depends):
+        self.name = name
+        self.depends = set(depends)
+        self.value = value
+
+
+def get_batches(nodes):
+    "Batches are sets of tasks that can be run together"
+
+    name_to_instance = dict((node.name, node) for node in nodes)
+    name_to_deps = dict((node.name, set(node.depends)) for node in nodes)
+    batches = []
+
+    while name_to_deps:
+        ready = dict(
+            (name, deps)
+            for name, deps
+            in name_to_deps.iteritems()
+            if not deps
+        )
+        if not ready:
+            msg = "Circular dependencies found!\n"
+            msg += format_dependencies(name_to_deps)
+            raise ValueError(msg)
+
+        for name in ready:
+            del name_to_deps[name]
+
+        for deps in name_to_deps.itervalues():
+            deps.difference_update(ready)
+
+        batches.append(dict(name_to_instance[name] for name in ready))
+
+    return batches
+
+
+def format_dependencies(name_to_deps):
+    msg = []
+    for name, deps in name_to_deps.iteritems():
+        for parent in deps:
+            msg.append("%s -> %s" % (name, parent))
+    return "\n".join(msg)
+
+
+def format_nodes(nodes):
+    return format_dependencies(dict(
+        (node.name, node.depends) for node in nodes
+    ))
